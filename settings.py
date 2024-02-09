6819ec71a41147893eeb52bb62d3b1c0cc034a66
Soheb Mandhai <42643338+hirizon@users.noreply.github.com>
2024-02-09 20:24:39 +0000

    Incorporated more settings into the file

--- settings.py
+++ settings.py
@@ -7,13 +7,20 @@ Initialise directories for testing among other variable components.
 @author: Soheb Mandhai
 """
 
+override_src = "J202505-540405"#'J202254-540537'
+
 prefix= "../sample_data/"#"/mnt/shared/"
 dataloc=prefix+"des/askap_data/"#'/mnt/shared/des/askap_data/' #askap data location
 WISEfiles_dir=prefix+'des/WISEtiles/*.fits' #WISE raw data tiles location
 DESfiles_dir=prefix + 'des/DEStiles/' #WISE raw data tiles location
 
 SB='9351'
-cat_sub = "catalogue/AS101_Continuum_Component_Catalogue_{}_78.csv"#'catalogues/AS101_Continuum_Component_Catalogue_{}_78.csv'
+island=True
+if island:
+    cat_sub = "catalogue/AS101_Continuum_Island_Catalogue_{}.csv"#'catalogues/AS101_Continuum_Component_Catalogue_{}_78.csv'
+else:
+    cat_sub = "catalogue/AS101_Continuum_Component_Catalogue_{}_78.csv"#'catalogues/AS101_Continuum_Component_Catalogue_{}_78.csv'
+
 radio_output=prefix+'des/radio_cutouts/SB'
 overlayloc = prefix+'des/radio_cutouts/SB'
 WISEtiles=  'WISE_tiles.txt'
