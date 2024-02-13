ff229cfb1bd26840cffb4f80ec8374e0b9a2d22b
Soheb Mandhai <42643338+smandhai@users.noreply.github.com>
2024-02-13 20:15:57 +0000

    Added more settings and labelling

--- settings.py
+++ settings.py
@@ -7,15 +7,26 @@ Initialise directories for testing among other variable components.
 @author: Soheb Mandhai
 """
 
-override_src = "J202505-540405"#'J202254-540537'
+# =============================================================================
+# Source Settings
+# =============================================================================
+
+override_src = ["J210240-552627","J202710-530638","J202505-540405","J202254-540537","J202715-553043"]#"J202505-540405"#'J202254-540537' #Default =None
+
+# =============================================================================
+# Field Settings
+# =============================================================================
+SB='9351'
+island=True
+# =============================================================================
+# Directories
+# =============================================================================
 
 prefix= "../sample_data/"#"/mnt/shared/"
 dataloc=prefix+"des/askap_data/"#'/mnt/shared/des/askap_data/' #askap data location
 WISEfiles_dir=prefix+'des/WISEtiles/*.fits' #WISE raw data tiles location
 DESfiles_dir=prefix + 'des/DEStiles/' #WISE raw data tiles location
 
-SB='9351'
-island=True
 if island:
     cat_sub = "catalogue/AS101_Continuum_Island_Catalogue_{}.csv"#'catalogues/AS101_Continuum_Component_Catalogue_{}_78.csv'
 else:
@@ -25,3 +36,8 @@ radio_output=prefix+'des/radio_cutouts/SB'
 overlayloc = prefix+'des/radio_cutouts/SB'
 WISEtiles=  'WISE_tiles.txt'
 DEStiles= 'DES_tiles_dims.txt'
+
+# =============================================================================
+# Contour Settings
+# =============================================================================
+cont_limit = 6
