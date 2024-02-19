666c0085a4e06b57f14456399d65d63b95211272
Soheb Mandhai <42643338+hirizon@users.noreply.github.com>
2024-02-19 18:44:28 +0000

    New settings have been added + cutout creation settings

--- settings.py
+++ settings.py
@@ -10,8 +10,10 @@ Initialise directories for testing among other variable components.
 # =============================================================================
 # Source Settings
 # =============================================================================
-
+use_file= False# If True, set override_src to the file name of the source list
 override_src = ["J202715-553043","J202505-540405","J210240-552627","J202710-530638","J202254-540537"]#"J202505-540405"#'J202254-540537' #Default =None
+
+#Note: If you are using a file, ensure the source names are in the first column
 
 # =============================================================================
 # Field Settings
@@ -22,6 +24,7 @@ island=False#True
 # Directories
 # =============================================================================
 
+#INPUTS
 prefix= "../sample_data/"#"/mnt/shared/"
 dataloc=prefix+"des/askap_data/"#'/mnt/shared/des/askap_data/' #askap data location
 WISEfiles_dir=prefix+'des/WISEtiles/*.fits' #WISE raw data tiles location
@@ -31,13 +34,26 @@ if island:
     cat_sub = "catalogue/AS101_Continuum_Island_Catalogue_{}.csv"#'catalogues/AS101_Continuum_Component_Catalogue_{}_78.csv'
 else:
     cat_sub = "catalogue/AS101_Continuum_Component_Catalogue_{}_78.csv"#'catalogues/AS101_Continuum_Component_Catalogue_{}_78.csv'
-
-radio_output=prefix+'des/radio_cutouts/SB'
-overlayloc = prefix+'des/radio_cutouts/SB'
 WISEtiles=  'WISE_tiles.txt'
 DEStiles= 'DES_tiles_dims.txt'
+
+#OUTPUT
+radio_output=prefix+'des/radio_cutouts/SB'
+overlayloc = prefix+'des/radio_cutouts/SB'
+
+
+"For excluded sources"
+exclusion_list = prefix+"excluded_sources.csv"
+exclusion_dir = prefix+"des/excluded_radio_cutouts/"
 
 # =============================================================================
 # Contour Settings
 # =============================================================================
 cont_limit = 8
+
+# =============================================================================
+# Additional Options
+# =============================================================================
+create_cutout = True #Create cutout of default view?
+cutout_dir = prefix+'des/radio_cutouts_6x6/'
+use_cross = False
