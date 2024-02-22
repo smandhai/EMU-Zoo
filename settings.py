7da7000ce582bc08d5be1284c45a9bc1e6a36401
Soheb Mandhai <42643338+hirizon@users.noreply.github.com>
2024-02-22 00:25:20 +0000

    Added settings to control cutout plotting and output

--- settings.py
+++ settings.py
@@ -18,10 +18,10 @@ island=True#True
 # =============================================================================
 
 #INPUTS
-prefix= "../sample_data/"#"/mnt/shared/"
-dataloc=prefix+"des/askap_data/"#'/mnt/shared/des/askap_data/' #askap data location
-WISEfiles_dir=prefix+'des/WISEtiles/*.fits' #WISE raw data tiles location
-DESfiles_dir=prefix + 'des/DEStiles/' #WISE raw data tiles location
+prefix= "../sample_data/des/"#"/mnt/shared/"
+dataloc=prefix+"askap_data/"#'/mnt/shared/des/askap_data/' #askap data location
+WISEfiles_dir=prefix+'WISEtiles/*.fits' #WISE raw data tiles location
+DESfiles_dir=prefix + 'DEStiles/' #WISE raw data tiles location
 
 if island:
     cat_sub = "catalogue/AS101_Continuum_Island_Catalogue_{}.csv"#'catalogues/AS101_Continuum_Component_Catalogue_{}_78.csv'
@@ -31,20 +31,20 @@ WISEtiles=  'WISE_tiles.txt'
 DEStiles= 'DES_tiles_dims.txt'
 
 #OUTPUT
-radio_output=prefix+'des/radio_cutouts/SB'
-overlayloc = prefix+'des/radio_cutouts/SB'
+radio_output=prefix+'radio_cutouts/SB'
+overlayloc = prefix+'radio_cutouts/SB'
 
 
 "For excluded sources"
 exclusion_list = prefix+"excluded_sources.csv"
-exclusion_dir = prefix+"des/excluded_radio_cutouts/"
+exclusion_dir = prefix+"excluded_radio_cutouts/"
 
 # =============================================================================
 # Source Settings
 # =============================================================================
 use_file= True# If True, set override_src to the file name of the source list
 override_src = ["J202715-553043","J202505-540405","J210240-552627","J202710-530638","J202254-540537"]#"J202505-540405"#'J202254-540537' #Default =None
-override_src = prefix+'des/beta_test_subject_ids.txt'
+override_src = prefix+'beta_test_subject_ids.txt'
 
 #Note: If you are using a file, ensure the source names are in the first column
 
@@ -60,8 +60,9 @@ cont_limit = 8
 create_cutout = True #Create cutout of default view?
 export_fits_cutout=True #Create fits cutout?
 cutout_dir = prefix+'radio_cutouts_6x6/'
-use_cross = False
-
+use_cross = False #Put a cross_hair on 6x6 cutout sources
+skip_plotting = True #Default: False
+skip_cutout_plotting = True #Default: False
 # =============================================================================
 # ADVANCED OPTIONS - DO NOT CHANGE UNLESS YOU KNOW WHAT YOU'RE DOING
 # =============================================================================
