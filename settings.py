c7fb327c8ea2f120f34c5fbe0f2b81089655c393
Soheb Mandhai <42643338+hirizon@users.noreply.github.com>
2024-04-27 21:30:04 +0100

    File handling modifications and selavy convention implementation

--- settings.py
+++ settings.py
@@ -11,8 +11,8 @@ Initialise directories for testing among other variable components.
 # =============================================================================
 # Field Settings
 # =============================================================================
-SB="9442"#"9351"#"9434"#'9442'#"9351"#
-component_number="86"#78# 90#86#78# 86
+SB="9351"#"9351"#"9434"#'9442'#"9351"#
+component_number="78"#78# 90#86#78# 86
 # SB="9351"#"9434"#"9351"#"9434"#'9442'#"9351"#""
 # component_number=78
 cat_type="component_new"#"component_new"#"component_new"#"island"#True#True
@@ -40,8 +40,8 @@ WISEtiles=  'WISE_tiles.txt'
 DEStiles= 'DES_tiles_dims.txt'
 
 #OUTPUT
-radio_output=prefix+'radio_cutouts/SB'
-overlayloc = prefix+'radio_cutouts/SB'
+radio_output=prefix+'phase1_radio_cutouts/SB'
+overlayloc = prefix+'phase1_radio_cutouts/SB'
 
 
 "For excluded sources"
@@ -52,16 +52,18 @@ exclusion_dir = prefix+"excluded_radio_cutouts/"
 # Source Settings
 # =============================================================================
 use_file= True# If True, set override_src to the file name of the source list
-override_src = ["J202715-553043","J202505-540405","J210240-552627","J202710-530638","J202254-540537"]#"J202505-540405"#'J202254-540537' #Default =None
-#override_src = prefix+'beta_test_subject_ids.txt'
+#override_src = ["J202715-553043","J202505-540405","J210240-552627","J202710-530638","J202254-540537"]#"J202505-540405"#'J202254-540537' #Default =None
+override_src = prefix+'beta_test_subject_ids.txt'
 #override_src = None
 #override_src= "J203605-532105"#"J202710-530638"# "J203638-532628"#"J210102-582918"
 #Note: If you are using a file, ensure the source names are in the first column
-override_src= ["J211525-531726","J211547-532405",]#"J211507-501139"##9434
-override_src= ["J204554-510432","J204221-494947","J210524-531430","J204539-505452","J205505-502604","J211447-510910"]#"J211507-501139"##9287
-override_src= ["J210337-490023","J204310-510020","J210951-531001","J210957-531003","J211536-485429"]#"J211507-501139"##9287
-override_src = "J211229-533041"#"J205308-535913"#"J205913-481721"
+# override_src= ["J211525-531726","J211547-532405",]#"J211507-501139"##9434
+# override_src= ["J204554-510432","J204221-494947","J210524-531430","J204539-505452","J205505-502604","J211447-510910"]#"J211507-501139"##9287
+# override_src= ["J210337-490023","J204310-510020","J210951-531001","J210957-531003","J211536-485429"]#"J211507-501139"##9287
+# override_src = "J211229-533041"#"J205308-535913"#"J205913-481721"
 override_src = "duplicate_sources.dat" #<- Validation for duplicates
+override_src = prefix + "complexity_sources/SB{}_subject_list_PhaseI_RGZ-EMU_cpx_ab4000_majax_ab20.csv".format(SB)
+#override_src = prefix + "complexity_sources/subject_list_PhaseI_RGZ-EMU_cpx_ab4000_majax_ab20.csv".format(SB)
 # =============================================================================
 # Contour Settings
 # =============================================================================
@@ -73,12 +75,13 @@ cont_limit = 8
 remove_single_contours = True #Remove single contours outside of target?
 create_cutout = True #Create cutout of default view?
 export_fits_cutout=True #Create fits cutout?
-cutout_dir = prefix+'dupli_radio_cutouts_6x6/'
+cutout_dir = prefix+'phase1_radio_cutouts_6x6/'
 use_cross = True #Put a cross_hair on 6x6 cutout sources
 skip_plotting = True #Default: False
 skip_cutout_plotting = False #Default: False
 mask_value = 100 #Masked value
 duplicate_sources = "duplicate_sources.dat"
+selavy_convention = True#Uses the selavy lettering convention BUT needs to have this in the catalogue file, column before component names
 # =============================================================================
 # ADVANCED OPTIONS - DO NOT CHANGE UNLESS YOU KNOW WHAT YOU'RE DOING
 # =============================================================================
--- utils.py
+++ utils.py
@@ -10,18 +10,41 @@ Utilities to conduct basic operations
 import os
 import shutil as sh
 
+def to_abc(num):
+	"Only works for two letters"
+	reps = int(num/27)
+	string = ''
+	pre_num = 0
+	for r in range(reps+1):
+# 		print("String length: ",len(string))
+# 		print(r)
+		if (num>26):
+			num-=26
+			pre_num += 1
+# 		print(num)
+# 		print(pre_num)
+		string += chr(ord('`')+num)
+		if (pre_num > 0) & (len(string)>1):
+			string = string.replace(string[-2],chr(ord('`')+1),pre_num)#Sets previous letter as a
+		if pre_num > 26:
+			pre_num = 0
+				
+	return string
+#%%
 def make_dir(dirname):
-    if os.path.isdir(dirname)==False:
-        os.mkdir(dirname)
-        
-def extract_sources(src_fname,field_split=''):
-    temp_src = src_fname.split(field_split)[-1].split("_")[0] 
-    return temp_src
+	if os.path.isdir(dirname)==False:
+		os.mkdir(dirname)
+		
+def extract_sources(src_fname,field_split=' ',add_prefix='',ind=-1,return_suffix=True):
+	temp_src = add_prefix + src_fname.split(field_split)[ind].split("_")[0].split(".")[0]
+	if return_suffix:
+		return temp_src,src_fname.split(temp_src)[-1].split(".")[0]
+	return temp_src
 
 def move_file(src_fname,new_dir='',split="\\"):
-    if len(src_fname.split(split))>0:
-        src_fname_source = src_fname.split(split)[-1] #If the source is nested in other directories
-    else:
-        src_fname_source = src_fname #If the file is already in the root directory
-    sh.move(src_fname,new_dir+src_fname_source)
-    
+	if len(src_fname.split(split))>0:
+		src_fname_source = src_fname.split(split)[-1] #If the source is nested in other directories
+	else:
+		src_fname_source = src_fname #If the file is already in the root directory
+	sh.move(src_fname,new_dir+src_fname_source)
+	
